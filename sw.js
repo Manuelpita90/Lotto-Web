8aec5c4fa48df70b25e01dd0ebd8dac22d2a32f2
Manuelpita90 <manuelpita90@gmail.com>
2026-02-12 21:11:30 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-const CACHE_NAME = 'lotto-ia-v1';
+const CACHE_NAME = 'lotto-ia-v2';
 const urlsToCache = [
   './',
   './index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
 ];
 
 self.addEventListener('install', event => {
