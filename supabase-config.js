c2c8b447b36c45b6ea7099cf935d892c462afd92
Manuelpita90 <manuelpita90@gmail.com>
2026-02-15 11:05:47 -0400

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-const supabaseConfig = {
-    url: "https://ojmqvkzqwnexiellqlnj.supabase.co",
-    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qbXF2a3pxd25leGllbGxxbG5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5NDU4NjUsImV4cCI6MjA4NjUyMTg2NX0.8mRvyiQ43dQH01aGdezVbV8LwfA9YWlY2sjEsGvQhAY"
-};
-
-module.exports = supabaseConfig;
